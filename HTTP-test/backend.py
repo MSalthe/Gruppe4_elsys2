d06857574ad1be1be9dc2af91ebf1025841be4b5
Adele <adeleferger03@gmail.com>
2024-04-04 12:45:58 +0200

    Init

--- HTTP-test/backend.py
+++ HTTP-test/backend.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+import random
+import json
 
 app = Flask(__name__)
 
@@ -16,6 +18,13 @@ def handle_text():
     text_data = request.form['text']  # Assuming the data is sent as form data
     print(text_data)  # Print the received text to the console
     return 'Text received', 200  # Respond to the client that the text was received
+
+@app.route('/api/newbutton', methods=['GET'])
+def respond():
+    a = random.randint(-100,100)
+    returnvalue = "{\"tilt\": " + str(a) + "}"
+    print(returnvalue)
+    return returnvalue
 
 if __name__ == '__main__':
     app.run(debug=True)
