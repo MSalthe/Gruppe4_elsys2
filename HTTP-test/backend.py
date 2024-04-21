f1751aaec6eee006898960f002c4f48128a6bde5
sukhmnas <sukhmans@stud.ntnu.no>
2024-04-21 11:34:29 +0200

    Sukhman endret glass og graf kode

--- HTTP-test/backend.py
+++ HTTP-test/backend.py
@@ -1,22 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import random
 import json
+import time 
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('glass.html')
+    return render_template('forside.html')
 
-@app.route('/glass')
+@app.route('/glass.html')
 def glass_view():
     return render_template('glass.html')
 
-@app.route('/graf')
+@app.route('/graf.html')
 def graf_view():
     return render_template('graf.html')
 
-@app.route('/pasient')
+@app.route('/pasient.html')
 def pasient():
     return render_template('pasient.html')
 
@@ -32,11 +34,13 @@ def handle_text():
     return 'Text received', 200  # Respond to the client that the text was received
 
 @app.route('/api/newbutton', methods=['GET'])
-def respond():
-    a = random.randint(-100,100)
-    returnvalue = "{\"tilt\": " + str(a) + "}"
-    print(returnvalue)
-    return returnvalue
+def init():
+    tilt_angle = random.randint(-100, 100)  
+    graph_number = random.randint(0, 100)  
+    return jsonify({
+        "tilt": tilt_angle,
+        "number": graph_number
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
