400da3d520e6536f0ff582221b41c4db8cf20a44
sukhmnas <sukhmans@stud.ntnu.no>
2024-04-22 15:13:44 +0200

    Ny graf behandler

--- HTTP-test/backend.py
+++ HTTP-test/backend.py
@@ -6,9 +6,21 @@ import socket
 
 app = Flask(__name__)
 
+numbers = []
+
+
 @app.route('/')
 def index():
     return render_template('forside.html')
+    
+@app.route('/forside.html')
+def forside():
+    return render_template('forside.html')
+
+# Ruter til HTML sider under pasient
+@app.route('/pasient.html')
+def pasient():
+    return render_template('pasient.html')
 
 @app.route('/glass.html')
 def glass_view():
@@ -18,10 +30,16 @@ def glass_view():
 def graf_view():
     return render_template('graf.html')
 
-@app.route('/pasient.html')
-def pasient():
-    return render_template('pasient.html')
+# Ruter til HTML sider under behandler
+@app.route('/behandler.html')
+def behandler():
+    return render_template('behandler.html')
 
+@app.route('/grafBehandler')
+def graf_behandler_view(): 
+    return jsonify(numbers)
+
+# Generatorer
 @app.route('/api/hello', methods=['POST'])
 def hello():
     print('Hello, World!')
@@ -34,12 +52,14 @@ def handle_text():
     return 'Text received', 200  # Respond to the client that the text was received
 
 @app.route('/api/newbutton', methods=['GET'])
-def init():
-    tilt_angle = random.randint(-100, 100)  
-    graph_number = random.randint(0, 100)  
+def init(): 
+    tilt_angle = random.randint(-100, 100)
+    time.sleep(1)  # Adjust the frequency of generation
+    graph_number = random.randint(0, 100)
+    numbers.append(graph_number)
     return jsonify({
         "tilt": tilt_angle,
-        "number": graph_number
+        "number": graph_number,
     })
 
 if __name__ == '__main__':
